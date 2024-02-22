47c479ca2ce8f7e6653dc892928c989b74f7c79f
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2024-02-21 19:08:46 -0800

    0x02. AirBnB clone - MySQL

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, DateTime
 from datetime import datetime
 import models
 import uuid
--- models/review.py
+++ models/review.py
@@ -2,12 +2,12 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from models.place import Place
 from models.user import User
+from models.place import Place
 
 class Review(BaseModel, Base):
     __tablename__ = 'reviews'
 
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
